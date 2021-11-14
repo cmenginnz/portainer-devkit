6c854a0171246cadf67a61ad46ce09ea9545adbe
Simon Meng <simon.meng@portainer.io>
2021-11-14 21:52:40 +1300

    cacl global var DEV_MOD

--- devkit/scripts/libs/init/init_common_vars.sh
+++ devkit/scripts/libs/init/init_common_vars.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+_init_common_var_dev_mod() {
+  DEV_MODE=1
+  local dev_content="ref: refs/heads/dev"
+  local git_head_file="${WORKSPACE_PATH}/portainer-devkit/.git/HEAD"
+
+  if [ -f "${git_head_file}" ] && [ "$(cat ${git_head_file})" == "${dev_content}" ]; then
+    DEV_MODE=0
+  fi
+
+  debug "[devkit.sh/_init_dev_mod] DEV_MODE=${DEV_MODE}"
+}
+
 init_common_vars() {
   if [[ $CURRENT_FILE_PATH == *.vscode* ]]; then
     PROJECT_ROOT_PATH=$(dirname $(dirname $(dirname "$CURRENT_FILE_PATH")))
@@ -10,4 +22,6 @@ init_common_vars() {
 
   WORKSPACE_PATH=$(dirname "$PROJECT_ROOT_PATH")
   debug "[devkit.sh] [init_common_vars()] WORKSPACE_PATH=$WORKSPACE_PATH"
+
+  _init_common_var_dev_mod
 }
