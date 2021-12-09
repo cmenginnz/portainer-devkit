efcc6b24a849dedba18d6a7b3f66631bcc363126
Simon Meng <simon.meng@portainer.io>
2021-12-09 17:19:07 +1300

    wip

--- devkit/scripts/libs/rsync/rsync_project.sh
+++ devkit/scripts/libs/rsync/rsync_project.sh
@@ -21,6 +21,10 @@ _rsync_dist_files_local() {
 }
 
 _rsync_dist_files_remote() {
+
+  sshpass -p "$SSH_PASSWORD" \
+    ssh "$SSH_USER_REAL@${TARGET_IP} mkdir -p ${DLV_WORK_DIR}
+
   sshpass -p "$SSH_PASSWORD" \
     rsync -r \
       "$PROJECT_ROOT_PATH/dist/" \
