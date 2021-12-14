82158b6e2f9db59aa733ff767c79892fb27b1be9
Simon Meng <mcpacino@gmail.com>
2021-12-14 07:39:28 +0000

    wip

--- devkit/scripts/libs/tmux/tmux.sh
+++ devkit/scripts/libs/tmux/tmux.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-tmux_has_session() {
+_tmux_has_session() {
   local session_name="$1"
   tmux has-session -t "${session_name}" 2>$STDOUT 1>&2
 }
@@ -11,21 +11,20 @@ tmux_has_window() {
   tmux list-window -t "$session_name" -F "#{window_name}" | grep "$window_name"
 }
 
-tmux_has_dead_window() {
+_tmux_has_dead_window() {
   local session_name="$1"
   local window_name="$2"
   #tmux list-windows -t "${session_name}" -F "#{window_name}" -f "#{pane_dead}" 2>$STDOUT | grep "${window_name}" 2>$STDOUT 1>&2
-  tmux -V
   tmux list-windows -t "${session_name}" -F "#{window_name}" -f "#{pane_dead}" | grep "${window_name}" 
 }
 
-tmux_switch_client() {
+_tmux_switch_client() {
   local session_name="$1"
   tmux switch-client -t $session_name
 }
 
 # the window must in curren session
-tmux_kill_window() {
+_tmux_kill_window() {
   local window_name="$1"
   tmux kill-window -t $window_name
 }
@@ -34,9 +33,9 @@ tmux_kill_dead_window() {
   local session_name="$1"
   local window_name="$2"  
 
-  tmux_has_dead_window $session_name $window_name && 
-  tmux_switch_client $session_name && 
-  tmux_kill_window $window_name
+  _tmux_has_dead_window $session_name $window_name && 
+  _tmux_switch_client $session_name && 
+  _tmux_kill_window $window_name
 }
 
 tmux_new_window() {
@@ -44,20 +43,22 @@ tmux_new_window() {
   local window_name="$2"
   local cmd="$3"
 
-  echo "debug tmux new window"
-  tmux new-window -t "$session_name" -n "$window_name" "$cmd"
+  _tmux_init_server
 
-
-  # tmux_has_dead_window $session_name $window_name && 
-  # tmux_switch_client $session_name && 
-  # tmux_kill_window $window_name
+  if _tmux_has_session "${session_name}"; then
+    tmux new-window -t "$session_name" -n "$window_name" "$cmd"
+  else
+    tmux_switch_client $session_name && 
+    tmux new-session -s "$session_name" -n "$window_name" "$cmd"
+  fi
 }
 
-tmux_init_server() {
+_tmux_init_server() {
   tmux start \; set-option -g -s exit-empty off \; set-option -g remain-on-exit on
 }
 
-
-# misc vscode
-# ee/ce webpack portainer
-# agent k8s swarm docker 
+# session    window
+# ----------------------------
+# misc       vscode
+# ee/ce      webpack/portainer
+# agent      k8s/swarm/docker 
