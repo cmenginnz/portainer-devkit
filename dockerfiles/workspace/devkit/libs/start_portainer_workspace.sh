9bc2cd0f93c2692c944d0d55d632b1318b376500
Simon Meng <simon.meng@portainer.io>
2021-12-16 21:28:38 +1300

    wip

--- dockerfiles/workspace/devkit/libs/start_portainer_workspace.sh
+++ dockerfiles/workspace/devkit/libs/start_portainer_workspace.sh
@@ -3,5 +3,6 @@
 MOUNT_POINT="/home/workspace/src"
 
 start_portainer_workspace() {
+  ${MOUNT_POINT}/portainer-devkit/devkit/scripts/devkit.sh ensure network &&
   ${MOUNT_POINT}/portainer-devkit/devkit/scripts/devkit.sh ensure workspace "${PORTAINER_WORKSPACE}"
 }
