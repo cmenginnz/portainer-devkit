28f06f456cbacbb2a9e7ba7ec8acfd18d886d198
Simon Meng <simon.meng@portainer.io>
2022-01-12 11:42:23 +1300

    wip

--- devkit/scripts/libs/rpc/rpc_dlv_kill.sh
+++ devkit/scripts/libs/rpc/rpc_dlv_kill.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 rpc_dlv_kill() {
-  ( sshpass -p $SSH_PASSWORD ssh $SSH_USER_REAL@$TARGET_IP pkill dlv >/dev/null 2>&1 && sleep 1 ) || true
+  [[ "$PROJECT" == "portainer" ]] && local killee="/portainer" || local killee="/agent"
+  ( sshpass -p $SSH_PASSWORD ssh $SSH_USER_REAL@$TARGET_IP pkill $killee >/dev/null 2>&1 && sleep 1 ) || true
 }
