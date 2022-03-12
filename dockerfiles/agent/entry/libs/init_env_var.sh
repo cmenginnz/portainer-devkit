44a702624951acf995397548cd94217d9b231cfc
Simon Meng <simon.meng@portainer.io>
2022-03-12 22:38:11 +1300

    wip

--- dockerfiles/agent/entry/libs/init_env_var.sh
+++ dockerfiles/agent/entry/libs/init_env_var.sh
@@ -1,6 +1,6 @@
 init_env_var() {
   mkdir -p /root/.ssh
-  # echo "" > /root/.ssh/environment;
+  echo "# portainer-devkit envs" >> /root/.ssh/environment;
   env | grep NOMAD >> /root/.ssh/environment;
   env | grep KUBERNETES >> /root/.ssh/environment;
 }
