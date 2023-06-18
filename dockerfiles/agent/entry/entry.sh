29dbe6a235ab35ac27b89f1adb0f078b57a3f1a9
cmeng <cmenginnz@gmail.com>
2023-06-18 17:33:37 +1200

    wip

--- dockerfiles/agent/entry/entry.sh
+++ dockerfiles/agent/entry/entry.sh
@@ -2,10 +2,12 @@
 
 [[ "${DEVKIT_DEBUG}" == "true" ]] &&  set -x
 
-source /entry/libs/start_sshd.sh
 source /entry/libs/init_root_pw.sh
+source /entry/libs/start_sshd.sh
+source /entry/libs/init_env_var.sh
 
 init_root_pw
 start_sshd
+init_env_var
 
 sleep infinity
